6d83cc9eba8669f1c1ae42a7ab68e3e50eec33ab
Duc Khai <duckhai0623@gmail.com>
2021-12-02 21:27:40 +0700

    fix: fix limited socket port in getReview of TikiAPI

--- Server/src/com/tiki_server/util/TikiAPI.java
+++ Server/src/com/tiki_server/util/TikiAPI.java
@@ -182,23 +182,26 @@ public class TikiAPI {
             String content = matcher.replaceAll("");
             content = content.replaceAll("\\xF0\\x9D\\x99\\xBA\\xE1\\xBA...","");
             review.setContent(content);
-
-            if(reviewBLL.findById(review.getId()) == null) {
-                if(productBLL.findById(review.getProductId()) != null) {
-                    reviewBLL.save(review);
-                    System.out.println("Lưu thành công review mới có review_id = " + review.getId());
-                } else
-                    System.out.println("Không tìm thấy product_id = " + review.getProductId() + " để lưu review");
-            } else {
-                ReviewDTO oldReview = reviewBLL.findById(review.getId());
-                if(review.getCommentCount().compareTo(oldReview.getCommentCount()) != 0
-                    || review.getStatus().compareTo(oldReview.getStatus()) !=  0
-                    || (review.getImageUrl() == null ? "" : review.getImageUrl()).compareTo(oldReview.getImageUrl() == null ? "" : oldReview.getImageUrl()) != 0
-                    || review.getRating() != oldReview.getRating()
-                    || review.getTitle().compareTo(oldReview.getTitle()) != 0) {
-                    reviewBLL.update(review);
-                    System.out.println("Update thành công review_id = "+review.getId());
+            try {
+                if (reviewBLL.findById(review.getId()) == null) {
+                    if (productBLL.findById(review.getProductId()) != null) {
+                        reviewBLL.save(review);
+                        System.out.println("Lưu thành công review mới có review_id = " + review.getId());
+                    } else
+                        System.out.println("Không tìm thấy product_id = " + review.getProductId() + " để lưu review");
+                } else {
+                    ReviewDTO oldReview = reviewBLL.findById(review.getId());
+                    if (review.getCommentCount().compareTo(oldReview.getCommentCount()) != 0
+                            || review.getStatus().compareTo(oldReview.getStatus()) != 0
+                            || (review.getImageUrl() == null ? "" : review.getImageUrl()).compareTo(oldReview.getImageUrl() == null ? "" : oldReview.getImageUrl()) != 0
+                            || review.getRating() != oldReview.getRating()
+                            || review.getTitle().compareTo(oldReview.getTitle()) != 0) {
+                        reviewBLL.update(review);
+                        System.out.println("Update thành công review_id = " + review.getId());
+                    }
                 }
+            } catch (Exception e) {
+                continue;
             }
         }
 
@@ -211,33 +214,37 @@ public class TikiAPI {
         JsonNode data = rootNode.get("data");
         for(JsonNode dataArray : data) {
             JsonNode comments = dataArray.get("comments");
-                for (JsonNode commentArray : comments) {
-                    if (commentArray != null) {
-                        CommentDTO comment = objectMapper.treeToValue(commentArray, CommentDTO.class);
-                        String regex = "[^\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]";
-                        Pattern pattern = Pattern.compile(regex, Pattern.UNICODE_CHARACTER_CLASS);
-                        Matcher matcher = pattern.matcher(comment.getContent());
-                        String content = matcher.replaceAll("");
-                        comment.setContent(content);
-                        if (reviewBLL.findById(comment.getReviewId()) == null)
-                            System.out.println("Không tìm thấy review id = " + comment.getReviewId() + " để lưu comment_id = " + comment.getId());
-                        else {
-                            if (commentBLL.findById(comment.getId()) == null) {
-                                commentBLL.save(comment);
-                                System.out.println("Lưu thành công comment mới có comment_id = " + comment.getId());
-                            } else {
-                                CommentDTO oldComment = commentBLL.findById(comment.getId());
-                                if ((comment.getAvatarUrl() == null ? "" : comment.getAvatarUrl()).compareTo(oldComment.getAvatarUrl() == null ? "" : oldComment.getAvatarUrl()) != 0
-                                        || comment.getCommentator().compareTo(oldComment.getCommentator()) != 0
-                                        || comment.getFullname().compareTo(oldComment.getFullname()) != 0
-                                        || comment.getReported().compareTo(oldComment.getReported()) != 0
-                                        || comment.getStatus() != oldComment.getStatus()) {
-                                    commentBLL.update(comment);
-                                    System.out.println("Update thành công comment mới có comment_id = " + comment.getId());
+                try {
+                    for (JsonNode commentArray : comments) {
+                        if (commentArray != null) {
+                            CommentDTO comment = objectMapper.treeToValue(commentArray, CommentDTO.class);
+                            String regex = "[^\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]";
+                            Pattern pattern = Pattern.compile(regex, Pattern.UNICODE_CHARACTER_CLASS);
+                            Matcher matcher = pattern.matcher(comment.getContent());
+                            String content = matcher.replaceAll("");
+                            comment.setContent(content);
+                            if (reviewBLL.findById(comment.getReviewId()) == null)
+                                System.out.println("Không tìm thấy review id = " + comment.getReviewId() + " để lưu comment_id = " + comment.getId());
+                            else {
+                                if (commentBLL.findById(comment.getId()) == null) {
+                                    commentBLL.save(comment);
+                                    System.out.println("Lưu thành công comment mới có comment_id = " + comment.getId());
+                                } else {
+                                    CommentDTO oldComment = commentBLL.findById(comment.getId());
+                                    if ((comment.getAvatarUrl() == null ? "" : comment.getAvatarUrl()).compareTo(oldComment.getAvatarUrl() == null ? "" : oldComment.getAvatarUrl()) != 0
+                                            || comment.getCommentator().compareTo(oldComment.getCommentator()) != 0
+                                            || comment.getFullname().compareTo(oldComment.getFullname()) != 0
+                                            || comment.getReported().compareTo(oldComment.getReported()) != 0
+                                            || comment.getStatus() != oldComment.getStatus()) {
+                                        commentBLL.update(comment);
+                                        System.out.println("Update thành công comment mới có comment_id = " + comment.getId());
+                                    }
                                 }
                             }
                         }
                     }
+                } catch (Exception e) {
+                    continue;
                 }
         }
     }
@@ -251,28 +258,32 @@ public class TikiAPI {
         for(JsonNode dataArray : data) {
             Long reviewId = dataArray.get("id").asLong();
             JsonNode timelines = dataArray.get("timeline");
-            if(timelines != null) {
-                TimelineDTO timeline = objectMapper.treeToValue(timelines, TimelineDTO.class);
-                timeline.setReviewId(reviewId);
-                String reviewCreatedDate = simpleDateFormat.format(timeline.getReviewCreatedDate());
-                timeline.setReviewCreatedDate(simpleDateFormat.parse(reviewCreatedDate));
-                String deliveryDate = simpleDateFormat.format(timeline.getDeliveryDate());
-                timeline.setDeliveryDate(simpleDateFormat.parse(deliveryDate));
-                if(reviewBLL.findById(timeline.getReviewId()) == null)
-                    System.out.println("Không tìm thấy review_id = "+timeline.getReviewId()+" để lưu timeline");
-                else {
-                    TimelineDTO oldTimeline = timelineBLL.findByReviewId(reviewId);
-                    if(oldTimeline == null) {
-                        timelineBLL.save(timeline);
-                        System.out.println("Lưu thành công timeline mới có review_id = " + timeline.getReviewId());
-                    } else if(timeline.getReviewCreatedDate().compareTo(oldTimeline.getReviewCreatedDate()) != 0
-                    || timeline.getDeliveryDate().compareTo(oldTimeline.getDeliveryDate()) != 0
-                    || timeline.getContent().compareTo(oldTimeline.getContent()) != 0
-                    || timeline.getExplaination().compareTo(oldTimeline.getExplaination()) != 0) {
-                        timelineBLL.updateByReviewId(timeline);
-                        System.out.println("Update thành công timeline mới có review_id = " + timeline.getReviewId());
+            try {
+                if (timelines != null) {
+                    TimelineDTO timeline = objectMapper.treeToValue(timelines, TimelineDTO.class);
+                    timeline.setReviewId(reviewId);
+                    String reviewCreatedDate = simpleDateFormat.format(timeline.getReviewCreatedDate());
+                    timeline.setReviewCreatedDate(simpleDateFormat.parse(reviewCreatedDate));
+                    String deliveryDate = simpleDateFormat.format(timeline.getDeliveryDate());
+                    timeline.setDeliveryDate(simpleDateFormat.parse(deliveryDate));
+                    if (reviewBLL.findById(timeline.getReviewId()) == null)
+                        System.out.println("Không tìm thấy review_id = " + timeline.getReviewId() + " để lưu timeline");
+                    else {
+                        TimelineDTO oldTimeline = timelineBLL.findByReviewId(reviewId);
+                        if (oldTimeline == null) {
+                            timelineBLL.save(timeline);
+                            System.out.println("Lưu thành công timeline mới có review_id = " + timeline.getReviewId());
+                        } else if (timeline.getReviewCreatedDate().compareTo(oldTimeline.getReviewCreatedDate()) != 0
+                                || timeline.getDeliveryDate().compareTo(oldTimeline.getDeliveryDate()) != 0
+                                || timeline.getContent().compareTo(oldTimeline.getContent()) != 0
+                                || timeline.getExplaination().compareTo(oldTimeline.getExplaination()) != 0) {
+                            timelineBLL.updateByReviewId(timeline);
+                            System.out.println("Update thành công timeline mới có review_id = " + timeline.getReviewId());
+                        }
                     }
                 }
+            } catch (Exception e) {
+                continue;
             }
         }
     }
